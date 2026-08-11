228ba3e9c2ce0dc07e84afd81444345d5b178953
Prakash Poudel <prakash@truemark.com.np>
2026-08-12 02:43:50 +0545

    chore: update storybook dependencies

---
+++ apps/web/.storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+
+const config: StorybookConfig = {
+  stories: ['../**/*.stories.@(js|jsx|mjs|ts|tsx)', '../../../packages/ui/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-docs'
+  ],
+  framework: '@storybook/nextjs',
+  docs: { autodocs: true },
+  staticDirs: ['../public']
+}
+
+export default config
